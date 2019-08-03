1fbab791a56eb572cbbacfeff2465ab40ed3c961
heroes-coding <jeremy@heroes.report>
2019-08-03 16:37:40 -0400

    hmm

--- src/sqs.js
+++ src/sqs.js
@@ -6,9 +6,12 @@ var params = {
 	QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/441215765077/replays', /* required */
 	MaxNumberOfMessages: 10
 }
+
+#const getSQSReplays = ()
+
 sqs.receiveMessage(params, function (err, data) {
 	if (err) console.log(err, err.stack); // an error occurred
-	else console.log(data);           // successful response
+	else console.log(len(data.Messages));           // successful respons
 })
 
 
